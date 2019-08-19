6939f4a7093fdeda06dbe7dd504739b1a4a03728
Noeuclides <euclidesnoeuclides@gmail.com>
2019-08-19 23:16:45 +0000

    setting /etc/nginx/sites-enabled/default file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,20 +2,11 @@
 # set up web servers for the deployment of web_static
 apt-get update -y
 apt-get install nginx -y
-mkdir /data/
-mkdir /data/web_static/
-mkdir /data/web_static/releases/
-mkdir /data/web_static/shared/
-mkdir /data/web_static/releases/test/
-touch /data/web_static/releases/test/index.html
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
 echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
-ln -s /data/web_static/releases/test/ /data/web_static/current
-chmod -R 664 /data/ 
-server {
-    location /hbnb_static {
-        alias /data/web_static/current/hbnb_static;
-    }
-}
+rm /data/web_static/current 
+ln -s /data/web_static/releases/test/ /data/web_static/current 
+sudo chown -R ubuntu:ubuntu /data 
+sed -i '37a\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/hbnb_static;\n\t}' /etc/nginx/sites-enabled/default
 service nginx restart
-
-
