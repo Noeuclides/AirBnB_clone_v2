481c7090afe03d195e7b3ce2675150f866a2eede
Noeuclides <euclidesnoeuclides@gmail.com>
2019-08-20 01:36:00 +0000

    fabric creating .tgz file with versions of web_static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from datetime import datetime
+from fabric.api import *
+'''fabric script to fgenerate tgz archive
+'''
+
+
+def do_pack():
+    '''making a pack on web_static folder
+    '''
+    now = datetime.now()
+    file = 'web_static_' + now.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    check = local("tar -cvzf versions/{file} web_static".format(file=file))
+    if check is not None:
+        return file
+    else:
+        return None
