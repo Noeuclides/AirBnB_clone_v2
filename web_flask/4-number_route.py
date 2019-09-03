0938383afef29d341681b52eb06b265bf4dfee1b
Noeuclides <euclidesnoeuclides@gmail.com>
2019-09-03 14:14:15 +0000

    Flask type integer

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+'''start a Flask web app
+'''
+from flask import Flask as fsk
+from flask import redirect, url_for, abort
+
+app = fsk(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    '''root path
+    '''
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb')
+def hbnb():
+    '''hbnb path
+    '''
+    return ("HBNB")
+
+
+@app.route('/c/<text>')
+def c_path(text):
+    '''c plus text
+    '''
+    text.replace('_', ' ')
+    return ("C {}".format(text))
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python_path(text=None):
+    '''python plus text
+    '''
+    if text is None:
+        return(redirect(url_for('python_path', text='is cool')))
+    else:
+        text.replace('_', ' ')
+        return ("Python {}".format(text))
+
+
+@app.route('/number/<n>')
+def number(n):
+    '''number path
+    '''
+    try:
+        return("{} is a number".format(int(n)))
+    except: 
+        abort(404)   
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
