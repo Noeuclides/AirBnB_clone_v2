7502da9b93f648cd9d8c28a05e2d2a5c9f6f9a91
Noeuclides <euclidesnoeuclides@gmail.com>
2019-09-05 00:58:32 +0000

    cities states list

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+'''start a Flask web app
+'''
+from flask import Flask as fsk
+from flask import render_template
+from models import storage
+
+app = fsk(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/states_list')
+def states_list():
+    '''list states
+    '''
+    state_dict = storage.all("State")
+    return(render_template('7-states_list.html', states=state_dict))
+
+
+@app.route('/cities_by_states')
+def cities_list():
+    '''list cities in state
+    '''
+    state_dict = storage.all("State")
+    cities_dict = storage.all("City")
+    return(render_template('8-cities_by_states.html',
+                           cities=cities_dict, states=state_dict))
+
+
+@app.teardown_appcontext
+def close_(exception=None):
+    """close session
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
